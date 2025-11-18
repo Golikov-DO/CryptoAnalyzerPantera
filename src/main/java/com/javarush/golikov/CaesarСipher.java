64035db58abdd1971baa6bb4a6486c070f36e57e
Your Name <your_email@whatever.com>
2025-11-18 09:05:56 +0300

    ADD variable inputText
    Used to store the transmitted text

--- src/main/java/com/javarush/golikov/CaesarСipher.java
+++ src/main/java/com/javarush/golikov/CaesarСipher.java
@@ -4,4 +4,9 @@ public class CaesarСipher {
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+    private char[] inputText;
+
+    public CaesarСipher(char[] inputText) {
+        this.inputText = inputText;
+    }
 }
