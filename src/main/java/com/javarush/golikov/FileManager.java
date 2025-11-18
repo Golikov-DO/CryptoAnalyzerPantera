7b53e20e6174f215e13259399d8496162159a8c2
Your Name <your_email@whatever.com>
2025-11-18 11:01:27 +0300

    ADD Logic for reading and writing from a file
    Implemented reading and writing from files

--- src/main/java/com/javarush/golikov/FileManager.java
+++ src/main/java/com/javarush/golikov/FileManager.java
@@ -1,11 +1,35 @@
 package com.javarush.golikov;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileManager {
+    Validator validator = new Validator();
+
+    // Логика чтения файла
     public String readFile(String filePath) {
-        return  null;
-        // Логика чтения файла
+        String fileContent;
+        Path path = Paths.get(filePath);
+        try {
+            byte[] encoded = Files.readAllBytes(path);
+            fileContent = new String(encoded, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return fileContent;
     }
+
+    // Логика записи файла
     public void writeFile(String content, String filePath) {
-        // Логика записи файла
+            Path path = Paths.get(filePath);
+            try {
+                Files.write(path, content.getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
     }
 }
