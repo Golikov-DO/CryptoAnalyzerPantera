79e94c6f4fc24380bdebe53441265c65a883b3d7
Your Name <your_email@whatever.com>
2025-11-18 09:28:37 +0300

    ADD to FileManager method readFile, writeFile
    Methods read or write to file.

--- src/main/java/com/javarush/golikov/FileManager.java
+++ src/main/java/com/javarush/golikov/FileManager.java
@@ -1,4 +1,11 @@
 package com.javarush.golikov;
 
 public class FileManager {
+    public String readFile(String filePath) {
+        return  null;
+        // Логика чтения файла
+    }
+    public void writeFile(String content, String filePath) {
+        // Логика записи файла
+    }
 }
