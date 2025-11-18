0923d4bfd2bcf45cf98558f521a02ddfacdb3e98
Your Name <your_email@whatever.com>
2025-11-18 09:23:11 +0300

    ADD menu display
    Shows a menu for selection.

--- src/main/java/com/javarush/golikov/ConsoleRunner.java
+++ src/main/java/com/javarush/golikov/ConsoleRunner.java
@@ -2,6 +2,12 @@ package com.javarush.golikov;
 
 public class ConsoleRunner {
     public static void main(String[] args) {
-
+        System.out.println("-------------------------");
+        System.out.println("1. Шифрование");
+        System.out.println("2. Расшифровка с ключом");
+        System.out.println("3. Brute force");
+        System.out.println("4. Статистический анализ");
+        System.out.println("0. Выход");
+        System.out.println("-------------------------");
     }
 }
