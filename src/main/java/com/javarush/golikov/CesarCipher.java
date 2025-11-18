4f3c97998bd7a656f6ab746c45eddf8890e733bb
Your Name <your_email@whatever.com>
2025-11-18 17:06:35 +0300

    ADD logic decrypt for CesarCipher
    The decryption logic has been implemented on CesarCipher decrypt

--- src/main/java/com/javarush/golikov/CesarCipher.java
+++ src/main/java/com/javarush/golikov/CesarCipher.java
@@ -33,7 +33,25 @@ public class CesarCipher {
 
     }
     public String decrypt(String encryptedText, int shift) {
-        return null;
+        int charPosition;
+        char[] result = new char[encryptedText.length()];
+        if (validator.isValidKey(shift, alphabet)) {
+            List<Character> charList = new ArrayList<>();
+            for (char c : alphabet) {
+                charList.add(c);
+            }
+            char[] textFromFile = encryptedText.toLowerCase().toCharArray();
+            for (int i = 0; i < textFromFile.length; i++) {
+                if (charList.contains(textFromFile[i])) {
+                    charPosition = charList.indexOf(textFromFile[i]);
+                    if (charPosition < shift) {
+                        result[i] = alphabet[(charList.size() - Math.abs(charPosition - shift) % charList.size())];
+                    } else result[i] = alphabet[(charPosition - shift) % charList.size()];
+                } else result[i] = textFromFile[i];
+            }
+        } else System.out.println("Неверный ключ шифрования");
+
+        return new String(result);
         // Логика расшифровки
     }
 }
