a44e6d9909b109704a308397f609561276ac8ea9
Your Name <your_email@whatever.com>
2025-11-18 09:26:31 +0300

    ADD Validator method isValidKey, isFileExists
    Methods check the correctness of the entered data.

--- src/main/java/com/javarush/golikov/Validator.java
+++ src/main/java/com/javarush/golikov/Validator.java
@@ -1,4 +1,12 @@
 package com.javarush.golikov;
 
 public class Validator {
+    public boolean isValidKey(int key, char[] alphabet) {
+        return false;
+        // Проверка ключа
+    }
+    public boolean isFileExists(String filePath) {
+        return false;
+        // Проверка существования файла
+    }
 }
