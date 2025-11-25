a016e486db9db82a127ccc54e4473167a1b59582
Xaxatumbik <xaxatumbik@gmail.com>
2025-11-25 18:40:37 +0300

    FIX ALPHABET. ADD decoding implementation.

--- src/main/java/com/javarush/golikov/menuComands/Decoding.java
+++ src/main/java/com/javarush/golikov/menuComands/Decoding.java
@@ -1,11 +1,29 @@
 package com.javarush.golikov.menuComands;
 
 import com.javarush.golikov.entite.Result;
+import com.javarush.golikov.exception.ApplicationExceptions;
 
-public class Decoding implements MenuCommands {
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Decoding extends Commands {
 
     @Override
     public Result execute(String[] parametrises) {
-        return new Result("Дешифрование завершено", "Ok");
+        String source = parametrises[0];
+        if (Files.isDirectory(Path.of(source))) {
+            source += "output.txt";
+        }
+        String target = parametrises[1];
+        if (Files.isDirectory(Path.of(target))) {
+            target += "decoding.txt";
+        }
+        int key;
+        try {
+            key = Integer.parseInt(parametrises[2]);
+        } catch (NumberFormatException e) {
+            throw new ApplicationExceptions("Неверный ключ");
+        }
+        return doCommand(source, target, key * -1);
     }
 }
